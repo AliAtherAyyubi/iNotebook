baacea589fbc0e4f3c16229d483b7ac6647b98c2
AliAtherAyyubi <info2atherayyubi@gmail.com>
2023-08-11 22:52:01 +0500

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,7 @@ import '../CSS/about.css'
 export default function About() {
   return (
     <>
+    {/* this is about page */}
     <div className="text-white aboutImg text-center">
                 <div className="note-img">
                     <h1 className="display-4">Empowering  Students</h1>
